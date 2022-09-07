afba721583cd449746827cbd099d6a06e98d28d8
Hichem <hichembouzourine.uni@gmail.com>
2022-09-08 00:34:28 +0100

    Handle Express async err without using Try catch.

--- api/index.js
+++ api/index.js
@@ -1,17 +1,22 @@
+require("express-async-errors");
 const express = require("express");
+const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const hotels = require("./routes/hotels");
 const users = require("./routes/users");
 const rooms = require("./routes/rooms");
 const auth = require("./routes/auth");
-const app = express();
 
 app.use(express.json());
 app.use("/api/hotels", hotels);
 app.use("/api/users", users);
 app.use("/api/rooms", rooms);
 app.use("/api/auth", auth);
+
+app.use((err, req, res, next) => {
+  res.status(500).send("Something failed internally");
+});
 
 dotenv.config();
 const db = process.env.MONGO;
