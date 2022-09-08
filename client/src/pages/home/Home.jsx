a4e2f4248a97c7e4cf12637e31c99705626ed011
Hichem <hichembouzourine.uni@gmail.com>
2022-09-09 00:52:19 +0100

    Update the UI by adding Properties by type.

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Inspirations from "../../components/featuredInspiration/Inspirations";
 import Header from "../../components/header/Header";
 import MailList from "../../components/mailList/MailList";
 import PropertyList from "../../components/PropertyList/PropertyList";
+import PropertyTypes from "../../components/propertyTypes/PropertyTypes";
 import "./home.css";
 
 const Home = () => {
@@ -12,7 +13,9 @@ const Home = () => {
       <div className="homeContainer">
         <Featured />
         <div className="homeTitle">
-          <h1>Browse by property type</h1>
+          <h1>Browse by property Type</h1>
+          <PropertyTypes />
+          <h1>Browse by property City</h1>
           <PropertyList />
           <h1>Get inspirations for your next trip</h1>
           <Inspirations />
